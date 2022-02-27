04f1008d23e04a249e7ea6a092bef2579c8f6308
zacharyrkenney <zacharykennedy@mail.weber.edu>
2022-02-27 15:53:54 -0700

    Added Imports for python file. Added comment depicting script author, date and project descritpion

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+    Author: Zachary Kennedy
+    Date: 2/27/2022
+    Desc:
+            This python scripts generates a sine curve with a random period, then removes a portion of the
+            data points making the period unrecognizeable. This script is meant to be used to test a
+            string length minimization script for accuracy.
+
+"""
+
+import math as m
+import matplotlib.pyplot as plt
